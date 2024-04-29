463deb9d385eacc6e3e5bd1a01eca491b3bd0dc8
bazzaou <hamza.bazzaou@etu.u-pec.fr>
2024-04-29 15:15:49 +0200

    ok sauvegarde

--- premierprogramme/SauvegardeController.java
+++ premierprogramme/SauvegardeController.java
@@ -1,23 +1,28 @@
+import javax.swing.*;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class SauvegardeController {
-    private static final String CHEMIN_FICHIER = "../fichier.gri";
-
     public void saveData(int[][] sudokuGridData) {
-        try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter(CHEMIN_FICHIER));
-            for (int i = 0; i < 9; i++) {
-                for (int j = 0; j < 9; j++) {
-                    writer.write(String.valueOf(sudokuGridData[i][j]));
+        JFileChooser fileChooser = new JFileChooser();
+        int userSelection = fileChooser.showSaveDialog(null);
+
+        if (userSelection == JFileChooser.APPROVE_OPTION) {
+            try {
+                String filePath = fileChooser.getSelectedFile().getAbsolutePath();
+                BufferedWriter writer = new BufferedWriter(new FileWriter(filePath));
+                for (int i = 0; i < 9; i++) {
+                    for (int j = 0; j < 9; j++) {
+                        writer.write(String.valueOf(sudokuGridData[i][j]));
+                    }
+                    writer.newLine();
                 }
-                writer.newLine();
+                writer.close();
+                System.out.println("Données sauvegardées avec succès.");
+            } catch (IOException e) {
+                System.err.println("Erreur lors de la sauvegarde des données : " + e.getMessage());
             }
-            writer.close();
-            System.out.println("Données sauvegardées avec succès.");
-        } catch (IOException e) {
-            System.err.println("Erreur lors de la sauvegarde des données : " + e.getMessage());
         }
     }
 }
