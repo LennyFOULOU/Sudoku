875aee4d208a96496a330d82499f34e10505ce3b
foulou.salle224-15 <lenny.foulou@etu.u-pec.fr>
2024-05-02 11:53:40 +0200

    suppression de classe inutile

--- deuxiemeprogramme/SudokuView.java
+++ deuxiemeprogramme/SudokuView.java
@@ -63,7 +63,6 @@ public class SudokuView {
         JMenu menu = new JMenu("Menu");
         JMenuItem menuItem = new JMenuItem("Retour au Menu");
         JMenuItem verifier = new JMenuItem("Vérifier");
-        JMenuItem sauvegarder = new JMenuItem("Sauvegarder");
         JMenuItem resoudre = new JMenuItem("Résoudre Sudoku"); 
 
 
@@ -85,15 +84,6 @@ public class SudokuView {
                 showMessage("La grille contient des erreurs.");
             }
         });
-
-        sauvegarder.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                SauvegardeController sauvegardeController = new SauvegardeController();
-                int[][] sudokuGridData = getSudokuGridData();
-                sauvegardeController.saveData(sudokuGridData);
-            }
-        });
         resoudre.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -105,7 +95,6 @@ public class SudokuView {
 
         menu.add(menuItem);
         menu.add(verifier);
-        menu.add(sauvegarder);
         menu.add(resoudre); 
         menuBar.add(menu);
         frame.setJMenuBar(menuBar);
